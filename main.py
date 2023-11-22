e8bd07bf175a5d71bd55b0b54d0b88f3a41d10d1
Joseph Clyde Mar <clydemar234@gmail.com>
2023-11-22 16:59:25 +0800

    Add search patterns for fractional exponents

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import re
-
-
+import math_operation
 
 def parse_expr(string_expr):
-    expression_search_pattern = r'([\+|\-])?([0-9]+|(\([0-9]+\/[0-9]+\)))?([\*|\/]?x)?(\^[0-9]+)?'
+    expression_search_pattern = r'([\+|\-])?([0-9]+|(\([0-9]+\/[0-9]+\)))?([\*|\/]?x)?(\^([0-9]+|(\([0-9]+\/[0-9]+\))))?'
     expression_search_regex = re.compile(expression_search_pattern)
 
-    term_coefficient_search_pattern = r'(^[\+|\-]?[0-9]*)'
+    term_coefficient_search_pattern = r'^[\+|\-]?([0-9]+|(\([0-9]+\/[0-9]+\)))?'
     term_coefficient_search_regex = re.compile(term_coefficient_search_pattern)
 
-    term_variable_search_pattern = r'x((\^[0-9]+)$)?'
+    term_variable_search_pattern = r'x((\^([0-9]+|(\([0-9]+\/[0-9]+\))))$)?'
     term_variable_search_regex = re.compile(term_variable_search_pattern)
 
     variable_search_pattern = r'^x'
     variable_search_regex = re.compile(variable_search_pattern)
 
-    exponent_search_pattern = r'\^[0-9]+'
+    exponent_search_pattern = r'\^([0-9]+|(\([0-9]+\/[0-9]+\)))'
     exponent_search_regex = re.compile(exponent_search_pattern)
 
     matched_terms = expression_search_regex.finditer(string_expr)
     parsed_expression = []
     for m in matched_terms:
-        print(m)
+        # print(m)
         if m.group() == '':
             continue
         term = []
@@ -47,3 +46,4 @@ def parse_expr(string_expr):
 expr = input('Enter expression: ')
 separated_expr = parse_expr(expr)
 print(separated_expr)
+# print(math_operation.differentiate(separated_expr))
