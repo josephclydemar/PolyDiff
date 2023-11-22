0369d36e45473ca1f0f59e4d4f9ad86bbf35fcd8
Joseph Clyde Mar <clydemar234@gmail.com>
2023-11-22 15:42:43 +0800

    Add regex for fractional coefficients

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 import re
 
-expression_search_pattern = r'([\+|\-])?([0-9]+|x)([\*|\/]x)?(\^[0-9]+)?'
-expression_search_regex = re.compile(expression_search_pattern)
 
-term_coefficient_search_pattern = r'(^[\+|\-]?[0-9]*)'
-term_coefficient_search_regex = re.compile(term_coefficient_search_pattern)
 
-term_variable_search_pattern = r'x((\^[0-9]+)$)?'
-term_variable_search_regex = re.compile(term_variable_search_pattern)
+def parse_expr(string_expr):
+    expression_search_pattern = r'([\+|\-])?([0-9]+|(\([0-9]+\/[0-9]+\)))?([\*|\/]?x)?(\^[0-9]+)?'
+    expression_search_regex = re.compile(expression_search_pattern)
 
-variable_search_pattern = r'^x'
-variable_search_regex = re.compile(variable_search_pattern)
+    term_coefficient_search_pattern = r'(^[\+|\-]?[0-9]*)'
+    term_coefficient_search_regex = re.compile(term_coefficient_search_pattern)
 
-exponent_search_pattern = r'\^[0-9]+'
-exponent_search_regex = re.compile(exponent_search_pattern)
+    term_variable_search_pattern = r'x((\^[0-9]+)$)?'
+    term_variable_search_regex = re.compile(term_variable_search_pattern)
 
-polynomial_input = input('Enter expression: ')
-matched_terms = expression_search_regex.finditer(polynomial_input)
+    variable_search_pattern = r'^x'
+    variable_search_regex = re.compile(variable_search_pattern)
 
-print(matched_terms)
+    exponent_search_pattern = r'\^[0-9]+'
+    exponent_search_regex = re.compile(exponent_search_pattern)
 
-for m in matched_terms:
-    # print(m)
-    temp = []
-    for i in term_coefficient_search_regex.finditer(m.group()):
-        x = i.group()
-        # print(len(x))
-        if len(x) == 0:
-            temp.append('+1')
-        elif len(x) == 1 and (x == '+' or x == '-'):
-            temp.append(f'{x}1')
-        else:
-            temp.append(x)
-    for i in term_variable_search_regex.finditer(m.group()):
-        for j in variable_search_regex.finditer(i.group()):
-            temp.append(j.group())
-        for j in exponent_search_regex.finditer(i.group()):
-            temp.append(j.group().replace('^', ''))
-        # temp.append(i.group())
-    print(temp)
+    matched_terms = expression_search_regex.finditer(string_expr)
+    parsed_expression = []
+    for m in matched_terms:
+        print(m)
+        if m.group() == '':
+            continue
+        term = []
+        for i in term_coefficient_search_regex.finditer(m.group()):
+            x = i.group()
+            # print(len(x))
+            if len(x) == 0:
+                term.append('+1')
+            elif len(x) == 1 and (x == '+' or x == '-'):
+                term.append(f'{x}1')
+            else:
+                term.append(x)
+        for i in term_variable_search_regex.finditer(m.group()):
+            for j in variable_search_regex.finditer(i.group()):
+                term.append(j.group())
+            for j in exponent_search_regex.finditer(i.group()):
+                term.append(j.group().replace('^', ''))
+        # print(term)
+        parsed_expression.append(term)
+    return parsed_expression
+
+
+expr = input('Enter expression: ')
+separated_expr = parse_expr(expr)
+print(separated_expr)
